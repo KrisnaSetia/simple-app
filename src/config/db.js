994c9ab43b05b3e69c7ecf1b5e51384aa82cd4d7
KrisnaSetia <147696945+KrisnaSetia@users.noreply.github.com>
2025-01-07 08:39:54 +0700

    fix: fix connection db

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,27 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
+require("dotenv").config(); // Memuat file .env
 const mysql= require("mysql");
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'dummy_telkomsel'
-});
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
 
-db.connect((err) => {
+  db.connect((err) => {
     if (err) {
-        console.error('Error connecting to database: ',err);
-    }else{
+      console.log("Connecting with:", {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+    
+      console.error("Error connecting to the database:", err);
+      return;
     }
-    console.log('Connected to database');
-});
+    console.log("Connected to the database successfully!");
+  });
 
 export default db;
