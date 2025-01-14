c6ac69e8ef55d05db498d1dfaaad675b3dcd4880
KrisnaSetia <147696945+KrisnaSetia@users.noreply.github.com>
2025-01-14 13:26:01 +0700

    style: fix style loading screen

--- src/components/LoadingScreen/index.tsx
+++ src/components/LoadingScreen/index.tsx
@@ -1,25 +1,19 @@
 import React from "react";
 import { Spinner, Container } from "react-bootstrap";
+import style from "./Loading.module.css"
 
 const LoadingSpinner = () => {
   return (
     <Container
       fluid
-      className="position-fixed top-0 start-0 h-100 w-100 d-flex flex-column align-items-center justify-content-center"
-      style={{
-        backgroundColor: "rgba(255, 255, 255, 0.8)",
-        zIndex: 9999,
-      }}
+      className={style.loadingContainer}
     >
       <Spinner
         animation="border"
         variant="danger"
-        style={{
-          width: "4rem",
-          height: "4rem",
-        }}
+        className={style.spinner}
       />
-      <span className="mt-3 fw-medium">Loading...</span>
+      <span className={style.spinnerContent}>Loading...</span>
     </Container>
   );
 };
